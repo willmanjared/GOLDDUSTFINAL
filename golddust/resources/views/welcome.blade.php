5fa52b2166cb9f7753f01afa46e96382a924983f
root <root@ip-172-31-36-144.us-west-2.compute.internal>
2017-09-09 09:27:03 +0000

    GENERATED SCAFFOLDING

--- golddust/resources/views/welcome.blade.php
+++ golddust/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    GOLDDUST
                 </div>
 
                 <div class="links">
