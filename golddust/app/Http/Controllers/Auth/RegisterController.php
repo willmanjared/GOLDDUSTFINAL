52d0f84db718bac7b48d3ade12e418dcecdf85e6
root <root@ip-172-31-36-144.us-west-2.compute.internal>
2017-09-13 11:18:08 +0000

    REDIS WONT WORK

--- golddust/app/Http/Controllers/Auth/RegisterController.php
+++ golddust/app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/f/dashboard';
 
     /**
      * Create a new controller instance.
--- golddust/resources/views/test/test.blade.php
+++ golddust/resources/views/test/test.blade.php
@@ -16,10 +16,11 @@
   <!-- Socket io -->
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
   <script>
-        //var socket = io('http://localhost:3000');
-        var socket = io('http://192.168.10.10:3000');
+        var socket = io('http://{{ Request::getHost() }}:3000');
+        //var socket = io('http://192.168.10.10:3000');
         socket.on("test-channel:App\\Events\\EventName", function(message){
             // increase the power everytime we load test route
+					console.log(message);
             $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
         });
     </script>
