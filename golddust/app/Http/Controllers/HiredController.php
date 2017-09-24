fcadb8c88842dcf31977e7d1a15b15ce560cd5da
root <root@ip-172-31-41-66.us-west-2.compute.internal>
2017-09-24 08:37:24 +0000

    ADDED HRIED AND ANSWERS MODELS

---
+++ golddust/app/Http/Controllers/HiredController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Hired;
+use Illuminate\Http\Request;
+
+class HiredController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Hired  $hired
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hired $hired)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Hired  $hired
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hired $hired)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Hired  $hired
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hired $hired)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Hired  $hired
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hired $hired)
+    {
+        //
+    }
+}
