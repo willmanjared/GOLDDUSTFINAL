e03226cf4becc07528738afed965db8b53931de8
root <root@ip-172-31-24-253.us-west-2.compute.internal>
2017-11-03 03:16:16 +0000

    broke register controller

--- golddust/app/Http/Controllers/Auth/RegisterController.php
+++ golddust/app/Http/Controllers/Auth/RegisterController.php
@@ -62,10 +62,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+      /*
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        */
+      dd('fix register controller');
     }
 }
